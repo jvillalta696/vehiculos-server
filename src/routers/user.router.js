e19090b453db298d450b3d2aa6987671f35ea112
jvillalta696 <jvillalta696@gmail.com>
2023-11-02 21:13:19 -0600

    add new route get all user

--- src/routers/user.router.js
+++ src/routers/user.router.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { create, deleteUsr, getByEmail } from "../controllers/auth.controller.js";
+import { create, deleteUsr, getAll, getByEmail } from "../controllers/auth.controller.js";
 
 export const userRouter = Router();
 
 userRouter.post('/',create);
 userRouter.get('/:email',getByEmail);
 userRouter.delete('/:uid',deleteUsr);
+userRouter.get('/',getAll);
